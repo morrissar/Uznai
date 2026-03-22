eb88c9953e5eef2b22550f23e65cf212924587d5
morrissar <morisarchik@gmail.com>
2026-03-22 15:45:55 +0800

    Update helper.py

--- пользователи/helper.py
+++ пользователи/helper.py
@@ -1,4 +1,3 @@
-import re
 from aiogram import Router, Bot, F
 from aiogram.types import Message
 import json
@@ -20,14 +19,9 @@ async def helper_reply(message: Message, bot: Bot):
     replied_msg = message.reply_to_message
     delete_map = load_json_map(DELETE_MAP_FILE)
     replied_msg_id = str(replied_msg.message_id)
-    
     if replied_msg_id in delete_map:
         user_id = delete_map[replied_msg_id]
-        await bot.send_message(
-            chat_id=int(user_id),
-            text=f'📨 <b>Ответ от администратора:</b>\n\n{message.text}',
-            parse_mode='HTML'
-        )
+        await bot.send_message(chat_id=int(user_id), text=f'📨 <b>Ответ от администратора:</b>\n\n{message.text}', parse_mode='HTML')
         await message.reply('✅ Ответ отправлен пользователю')
 
 @helper_router.message(F.chat.id == -1003710242278, F.message_thread_id == 2, F.reply_to_message)
@@ -35,13 +29,8 @@ async def post_reply(message: Message, bot: Bot):
     replied_msg = message.reply_to_message
     post_map = load_json_map(POST_MAP_FILE)
     replied_msg_id = str(replied_msg.message_id)
-    
     if replied_msg_id in post_map:
         user_id = post_map[replied_msg_id]
         if message.text:
-            await bot.send_message(
-                chat_id=int(user_id),
-                text=f'📨 <b>Ответ от администратора на ваш пост:</b>\n\n{message.text}',
-                parse_mode='HTML'
-            )
+            await bot.send_message(chat_id=int(user_id), text=f'📨 <b>Ответ от администратора на ваш пост:</b>\n\n{message.text}', parse_mode='HTML')
             await message.reply('✅ Ответ отправлен автору поста')
