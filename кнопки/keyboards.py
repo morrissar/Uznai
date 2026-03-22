5557b8a261d302838b0abf11ddc961dbcce6c4f7
morrissar <morisarchik@gmail.com>
2026-03-22 15:45:42 +0800

    Update keyboards.py

--- кнопки/keyboards.py
+++ кнопки/keyboards.py
@@ -5,21 +5,13 @@ main = ReplyKeyboardMarkup(keyboard=[
     [KeyboardButton(text='Предложить пост')],
     [KeyboardButton(text='Удалить пост')],
     [KeyboardButton(text='Контакты руководства')]
-], resize_keyboard=True,
-    input_field_placeholder='Выберите действие')
+], resize_keyboard=True, input_field_placeholder='Выберите действие')
 
-after_soglash = ReplyKeyboardMarkup(
-    keyboard=[
-    [KeyboardButton(text='Назад в меню')]
-    ], resize_keyboard=True, input_field_placeholder='Читайте внимательно')
+after_soglash = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text='Назад в меню')]], resize_keyboard=True)
 
-after_predloji = ReplyKeyboardMarkup( 
-    keyboard=[
-    [KeyboardButton(text='Назад в меню')]  
-    ], resize_keyboard=True, input_field_placeholder='Ждем ваши посты')
-
-after_udali = ReplyKeyboardMarkup(  
-    keyboard=[
+after_udali = ReplyKeyboardMarkup(keyboard=[
     [KeyboardButton(text='Платное удаление'), KeyboardButton(text='Бесплатное удаление')],
     [KeyboardButton(text='Назад в меню')]
-    ], resize_keyboard=True, input_field_placeholder='Подавайте заявку')
+], resize_keyboard=True, input_field_placeholder='Подавайте заявку')
+
+back_keyboard = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text='Назад в меню')]], resize_keyboard=True)
